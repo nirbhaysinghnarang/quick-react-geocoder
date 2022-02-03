788f5dee37a3b1a9190e008b720c29f29abc4db0
nirbhaysinghnarang <nsn8@cornell.edu>
2022-02-03 03:03:12 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [coords,setCoords] = useState({});
+  const api = {
+    key:"AIzaSyB-jZ-fJsZG_GPiVCAMPv12s7lcfZUFtQE",
+    baseURL:"https://maps.googleapis.com/maps/api/geocode/json?address="
+  }
+
+  const cleanUp = (num) => {
+    return Math.round(num * 100) / 100
+  }
+
+
+  const search = evt => {
+    if(evt.key==="Enter"){
+      console.log("Search init.")
+      const finalURL = api.baseURL+sanitiseInput(query)+"&key="+api.key;
+      fetch(finalURL)
+       .then(res => res.json())
+       .then(
+         result => {
+           console.log(result.results)
+           setCoords(result.results);
+           setQuery('');
+       })
+     }
+  }
+
+  const sanitiseInput = (input) => {
+    return input.replace(" ", "+");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {console.log(coords)}
+      <main>
+        <h1 className="title"> Quick React Geocoder </h1>
+        <input type="text" className="place-form" placeholder="Search for an address..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search}></input>
+        {(Object.keys(coords).length != 0) ? (
+          <div>
+            <h3 className="coords"> {cleanUp(coords[0].geometry.location.lat)}, {cleanUp(coords[0].geometry.location.lng)} </h3>
+            <h4 className = "address"> {coords[0].formatted_address} </h4>
+          </div>
+        ) : (
+            <h3 className="coords"> Enter address above </h3>
+        )
+        }
+      </main>
     </div>
   );
 }
